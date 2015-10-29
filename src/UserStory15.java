7f1302d29e9277a07fe3f3714f39750aa48844a5
haburka <edios124@gmail.com>
2015-10-29 15:39:50 -0400

    Added my user stories

---
+++ src/UserStory15.java
@@ -0,0 +1,85 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class UserStory15 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.foodchain.co/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUserStory15() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.id("name1")).clear();
+    driver.findElement(By.id("name1")).sendKeys("Rob");
+    driver.findElement(By.id("email1")).clear();
+    driver.findElement(By.id("email1")).sendKeys("edios124@gmail.com");
+    driver.findElement(By.id("Android")).click();
+    driver.findElement(By.id("submit1")).click();
+    driver.findElement(By.id("submit1")).click();
+    driver.findElement(By.id("name1")).clear();
+    driver.findElement(By.id("name1")).sendKeys("Rob");
+    driver.findElement(By.id("email1")).clear();
+    driver.findElement(By.id("email1")).sendKeys("edios124@gmail.com");
+    driver.findElement(By.id("Android")).click();
+    driver.findElement(By.id("submit1")).click();
+    assertTrue(isElementPresent(By.cssSelector("div.error")));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ src/UserStory6.java
@@ -0,0 +1,74 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class UserStory6 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.foodchain.co/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUserStory6() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.cssSelector("i.fa.fa-bars")).click();
+    driver.findElement(By.xpath("(//a[contains(text(),'Join Our Beta Test')])[3]")).click();
+    assertTrue(isElementPresent(By.cssSelector("#beta > div.container > div.row > div.col-md-12 > div.tittle > h1")));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
